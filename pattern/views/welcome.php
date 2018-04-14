fe8e9646a48bce453468ee819bc0458e546a4307
Navneil Naicker <navneil.naicker@gmail.com>
2018-04-14 13:22:59 +1200

    Change the documentation URL

--- pattern/views/welcome.php
+++ pattern/views/welcome.php
@@ -3,6 +3,6 @@
     <img class="img-circle" src="<?php echo public_url('images/Grumpy-Cat-920x584.jpg'); ?>" alt="">
     <h3>Hello Human!</h3>
     <p>Yes, you have successfully installed the Vow framework. Happy?</p>
-    <p><a href="https://github.com/navneil-naicker/vow-php-mvc" target="_blank" class="btn btn-danger"><i class="fa fa-book"></i> Read Docs</a></p>
+    <p><a href="https://github.com/navneil-naicker/scriptcha-mvc" target="_blank" class="btn btn-danger"><i class="fa fa-book"></i> Read Docs</a></p>
 </div>
 <?php view('footer'); ?>
